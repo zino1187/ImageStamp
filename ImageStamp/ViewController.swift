7b40c6f39ee8e798b5e6a6a95d6a5ac6d10ede27
zino <minzino@hanmail.net>
2020-02-10 22:32:40 +0900

    last commit

--- ImageStamp/ViewController.swift
+++ ImageStamp/ViewController.swift
@@ -9,12 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(viewClick))
+        
+        self.view.addGestureRecognizer(gesture)
     }
-
+    
+    @objc func viewClick(touch:UITapGestureRecognizer){
+        NSLog("뷰 클릭했니?", "")
+        createStamp(touch: touch)
+    }
+    
+    func createStamp(touch:UITapGestureRecognizer){
+        var imgView = UIImageView(image: UIImage(named: "h5.png"))
+        let point = touch.location(in: self.view)
+        imgView.frame = CGRect(x: point.x, y: point.y, width: 40, height: 40)
+        self.view.addSubview(imgView)
+    }
 
 }
 
